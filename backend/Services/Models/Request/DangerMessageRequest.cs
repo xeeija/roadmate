d862f78a28400b65e599b3c952a7460d3385f058
Xeeija <19950592+xeeija@users.noreply.github.com>
2023-10-21 15:14:28 +0200

    91: update DangerMessageController

--- backend/Services/Models/Request/DangerMessageRequest.cs
+++ backend/Services/Models/Request/DangerMessageRequest.cs
@@ -4,13 +4,14 @@ namespace Services.Models.Request;
 
 public class DangerMessageRequest : IRequestModel<DangerMessage> {
   public string Message { get; set; }
-  public Guid DangerId { get; set; }
+
+  // public Guid DangerId { get; set; }
   public Guid? ReferencedMessageId { get; set; }
 
   public DangerMessage ToEntity() {
     return new DangerMessage {
       Message = Message,
-      DangerId = DangerId,
+      // DangerId = DangerId,
       ReferencedMessageId = ReferencedMessageId
     };
   }
