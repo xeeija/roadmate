0663f948dbce158c3c2c91ee5fdcaffe97dac0c2
Felix Sailer <felix.sailer@edu.fh-joanneum.at>
2023-10-26 13:17:12 +0200

    76: change notification page

--- frontend/src/pages/Notifications.tsx
+++ frontend/src/pages/Notifications.tsx
@@ -21,21 +21,74 @@ const Notifications: React.FC = () => {
     <IonPage>
       <IonContent>
         <div className="toolbar-container">
-          <ToolBar title="Karte" backButton={true} />
+          <ToolBar title="Karte" />
         </div>
 
-        <IonCard color="light">
-          <IonCardHeader>
-            <IonText className="notifications-title">Benachrichtigungen</IonText>
-          </IonCardHeader>
+        <div className="rounded-modal">
+          <IonText className="notifications-title">Benachrichtigungen</IonText>
 
-          <div className="notifications-list">
-            <IonList style={{ color: "white" }}>
-              <Notification name="Unfall 1 gemeldet" date={new Date("2023-10-21T14:52:00")} route="Arbeitsweg" id={1} />
-              <Notification name="Unfall 2 gemeldet" date={new Date("2023-10-21T17:11:00")} route="Arbeitsweg" id={2} />
-            </IonList>
-          </div>
-        </IonCard>
+          <IonCard>
+            <div className="notifications-list">
+              <IonList style={{ color: "white" }}>
+                <Notification
+                  name="Unfall 1 gemeldet"
+                  date={new Date("2023-10-21T14:52:00")}
+                  route="Arbeitsweg"
+                  id={1}
+                />
+                <Notification
+                  name="Unfall 2 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+                {/* Add more notifications here */}
+                <Notification
+                  name="Unfall 3 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+                <Notification
+                  name="Unfall 4 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+                <Notification
+                  name="Unfall 5 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+                <Notification
+                  name="Unfall 6 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+                <Notification
+                  name="Unfall 7 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+                <Notification
+                  name="Unfall 8 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+                <Notification
+                  name="Unfall 9 gemeldet"
+                  date={new Date("2023-10-21T17:11:00")}
+                  route="Arbeitsweg"
+                  id={2}
+                />
+              </IonList>
+            </div>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
