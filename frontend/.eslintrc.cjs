e11cb4f07ff7826118b465356b70a8c6ffa10490
Xeeija <19950592+xeeija@users.noreply.github.com>
2023-10-27 19:46:14 +0200

    109: update (fix) eslint

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+  ],
+  plugins: [
+    "react",
+    "@typescript-eslint"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    project: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "quotes": ["warn", "double", { "allowTemplateLiterals": true }],
+    "jsx-quotes": ["warn", "prefer-double"],
+  },
+  ignorePatterns: [
+    "src/services/*",
+  ],
+}
