cdc2268f9188fd87bd54515a27dc27a1832217c8
Felix Sailer <felix.sailer@edu.fh-joanneum.at>
2023-11-01 21:42:31 +0100

    77: add new styling

--- frontend/src/components/DangerAcute.tsx
+++ frontend/src/components/DangerAcute.tsx
@@ -14,19 +14,20 @@ import { cogOutline, locationOutline, timeOutline } from "ionicons/icons"
 import "./DangerAcute.css"
 import { FC, useState } from "react"
 
+// Missing props in interface: title, description, position, lastUpdate, status
 interface DangerAcuteProps {
   closeModal: () => void
 }
 
 const DangerAcute: FC<DangerAcuteProps> = ({ closeModal }) => {
-  const [isModalOpen, setIsModalOpen] = useState(true)
+  const [isModalOpen] = useState(true)
 
   return (
     <>
       <IonModal isOpen={isModalOpen} animated={false}>
         <IonCard className="acute-danger-card" color="white">
           <IonCardHeader>
-            <IonCardTitle className="acute-danger-title">Unfall</IonCardTitle>
+            <IonCardTitle className="acute-danger-title">Unfall - Reitschulgasse</IonCardTitle>
           </IonCardHeader>
 
           <IonCardContent>
@@ -34,22 +35,28 @@ const DangerAcute: FC<DangerAcuteProps> = ({ closeModal }) => {
           </IonCardContent>
 
           <div className="description-list">
-            <IonItem lines="none">
+            <IonItem lines="full" className="ion-item-no-padding">
               <IonIcon icon={locationOutline} slot="start"></IonIcon>
               <p className="description-title">Position: &nbsp;</p>
-              <p className="description">Reitschulgasse 12</p>
+              <p className="description">
+                <b>Reitschulgasse 12</b>
+              </p>
             </IonItem>
 
-            <IonItem lines="none">
+            <IonItem lines="full">
               <IonIcon icon={timeOutline} slot="start"></IonIcon>
               <p className="description-title">Letzte Meldung: &nbsp;</p>
-              <p className="description">27.10.2023 um 15:00 Uhr</p>
+              <p className="description">
+                <b>27.10.2023 um 15:00 Uhr</b>
+              </p>
             </IonItem>
 
             <IonItem lines="none">
               <IonIcon icon={cogOutline} slot="start"></IonIcon>
               <p className="description-title">Status: &nbsp;</p>
-              <p className="description">Aktiv</p>
+              <p className="description">
+                <b>Aktiv</b>
+              </p>
               <div className="status"></div>
             </IonItem>
           </div>
