e4634f64184ebd4965650df8ef3eb7acd599ee3c
Xeeija <19950592+xeeija@users.noreply.github.com>
2023-11-26 23:49:16 +0100

    95: add own resolve request endpoint

--- backend/API/Controllers/DangerRequestController.cs
+++ backend/API/Controllers/DangerRequestController.cs
@@ -12,6 +12,9 @@ public class DangerRequestController : BaseController<DangerRequest, DangerReque
   private readonly DangerRequestService dangerRequestService;
   private readonly DangerService dangerService;
 
+  private const int createDangerThreshold = 2;
+  private const int resolveDangerThreshold = 2;
+
   public DangerRequestController(GlobalService service, IHttpContextAccessor accessor) :
     base(service.DangerRequestService, accessor) {
     dangerRequestService = service.DangerRequestService;
@@ -35,106 +38,91 @@ public class DangerRequestController : BaseController<DangerRequest, DangerReque
 
     // Find any nearby danger that is pending
     // and check if the request threshold is met already
-    var dangerThreshold = 2;
 
+    // TODO: Filter only recent danger requests (1-2h?)
+    var requestsInRange = await dangerRequestService.FindDangersInRadius(request.Lat, request.Lon, dr => dr.DangerId == null);
+    Log.Debug($"requests in range: {requestsInRange.Count} ({request.Lat}, {request.Lon})");
 
-    if (request.Type == RequestType.Create) {
-      // Create
-      // var dangersInRadius = await dangerRequestService.FindDangersInRadius(request.Lat, request.Lon);
+    // Create danger if enough requests are in range
+    var distinctCount = requestsInRange.DistinctBy(dr => dr.UserId).Count();
 
-      // var danger = dangersInRadius.Where(d => !d.IsActive && !d.IsResolved).FirstOrDefault();
+    if (distinctCount >= createDangerThreshold) {
 
-      // TODO: Filter only recent danger requests (1-2h?)
-      var requestsInRange = await dangerRequestService.FindDangersInRadius(request.Lat, request.Lon, dr => dr.DangerId == null);
-      Log.Debug($"requests in range: {requestsInRange.Count} ({request.Lat}, {request.Lon})");
+      var createdDanger = await dangerService.Create(new DangerModel {
+        Type = DangerType.Temporary,
+        // TODO: Middle point of all requests? Centroid or mean of coordinates maybe
+        Lat = request.Lat,
+        Lon = request.Lon,
+        // TODO: Category modal (most occurences)?
+        CategoryId = request.CategoryId,
+        Description = request.Description,
+        ActiveAt = DateTime.UtcNow
+      }.ToEntity());
 
-      // if (danger == null) {
-      //   result.Value.ErrorMessages.Add("Danger is null");
-      //   return BadRequest(result.Value);
-      // }
+      if (!createdDanger.IsAuthorized) {
+        createdDanger.ErrorMessages.Add("Unauthorized to create danger");
+        return BadRequest(createdDanger);
+      }
+      if (createdDanger.HasError) {
+        return BadRequest(createdDanger);
+      }
 
-      // request.DangerId = danger.ID;
+      if (result?.Data != null && createdDanger.Data != null) {
 
-      // Create danger if enough requests are in range
-      var distinctCount = requestsInRange.DistinctBy(dr => dr.UserId).Count();
-
-      if (distinctCount >= dangerThreshold) {
-        //   danger.ActiveAt = DateTime.UtcNow;
-        //   await dangerService.Update(danger);
-        // }
-
-        // else {
-
-        // Otherwise create a new pending danger
-
-        // if (request.CategoryId == null) {
-        //   result.Value.ErrorMessages.Add("CategoryID may not be null");
-        //   return BadRequest(result.Value);
-        // }
-
-        var createdDanger = await dangerService.Create(new DangerModel {
-          Type = DangerType.Temporary,
-          // TODO: Middle point of all requests? Centroid or mean of coordinates maybe
-          Lat = request.Lat,
-          Lon = request.Lon,
-          // TODO: Category modal (most occurences)?
-          CategoryId = request.CategoryId,
-          Description = request.Description,
-          ActiveAt = DateTime.UtcNow
-        }.ToEntity());
-
-        if (!createdDanger.IsAuthorized) {
-          createdDanger.ErrorMessages.Add("Unauthorized to create danger");
-          return BadRequest(createdDanger);
-        }
-        if (createdDanger.HasError) {
-          return BadRequest(createdDanger);
-        }
-
-        if (result?.Data != null && createdDanger.Data != null) {
-
-          // result.Value.Data.DangerId = createdDanger.Data.ID;
-          // await dangerRequestService.Update(result.Value.Data);
-
-          // TODO: Update many / UpdateRange -> BaseService
-          foreach (var dr in requestsInRange) {
-            dr.DangerId = createdDanger.Data.ID;
-            await dangerRequestService.Update(result.Data);
-          }
+        // TODO: Update many / UpdateRange -> BaseService
+        foreach (var dr in requestsInRange) {
+          dr.DangerId = createdDanger.Data.ID;
+          await dangerRequestService.Update(result.Data);
         }
       }
-    }
-    else {
-      // Resolve
-
-      var danger = await dangerService.Get(request.DangerId.ToString() ?? "", new() { "Requests" });
-
-      if (!danger.IsAuthorized) {
-        danger.ErrorMessages.Add("Unauthorized to create danger");
-        return BadRequest(danger);
-      }
-
-      if (danger.HasError) {
-        return BadRequest(danger);
-      }
-
-      if (danger.Data == null) {
-        result?.ErrorMessages.Add("Danger is null");
-        return BadRequest(result);
-      }
-
-      var resolveRequests = danger.Data.Requests.Where(dr => dr.Type == RequestType.Resolve);
-      var distinctCount = resolveRequests.DistinctBy(dr => dr.UserId).Count();
-
-      // Resolve danger
-      if (distinctCount >= dangerThreshold) {
-        danger.Data.ResolvedAt = DateTime.UtcNow;
-        await dangerService.Update(danger.Data);
-      }
-
     }
 
     return Created(result?.Data.ID.ToString() ?? "", result);
+
+  }
+
+  [HttpPost("Resolve")]
+  public async Task<ActionResult<ItemResponseModel<DangerRequest>>> Resolve([FromBody] DangerResolveRequestModel request) {
+
+    var danger = await dangerService.Get(request.DangerId.ToString() ?? "", new() { "Requests" });
+
+    if (!danger.IsAuthorized) {
+      danger.ErrorMessages.Add("Unauthorized to create danger");
+      return BadRequest(danger);
+    }
+
+    if (danger.HasError) {
+      return BadRequest(danger);
+    }
+
+    if (danger.Data == null) {
+      danger?.ErrorMessages.Add("Danger is null");
+      return BadRequest(danger);
+    }
+
+    request.CategoryId = danger.Data.CategoryId;
+
+    // Resolve
+    var result = await Service.Create(request.ToEntity());
+
+    if (!result.IsAuthorized) {
+      return Forbid();
+    }
+
+    if (result == null || result.Data == null || result.HasError) {
+      return BadRequest(result);
+    }
+
+    var resolveRequests = danger.Data.Requests.Where(dr => dr.Type == RequestType.Resolve);
+    var distinctCount = resolveRequests.DistinctBy(dr => dr.UserId).Count();
+
+    // Resolve danger
+    if (distinctCount >= resolveDangerThreshold) {
+      danger.Data.ResolvedAt = DateTime.UtcNow;
+      await dangerService.Update(danger.Data);
+    }
+
+    return Ok(result);
 
   }
 
--- backend/Services/Models/Request/DangerRequestModel.cs
+++ backend/Services/Models/Request/DangerRequestModel.cs
@@ -3,23 +3,23 @@
 namespace Services.Models.Request;
 
 public class DangerRequestModel : IRequestModel<DangerRequest> {
-  public RequestType Type { get; set; }
+  // public RequestType Type { get; set; }
   public Guid UserId { get; set; }
   public double Lat { get; set; }
   public double Lon { get; set; }
   public string Description { get; set; }
   public Guid CategoryId { get; set; }
   public DateTime? Timestamp { get; set; }
-  public Guid? DangerId { get; set; }
+  // public Guid? DangerId { get; set; }
 
   public DangerRequest ToEntity() {
     return new DangerRequest {
-      Type = Type,
+      Type = RequestType.Create,
       Timestamp = Timestamp ?? DateTime.UtcNow,
       UserId = UserId,
       Lat = Lat,
       Lon = Lon,
-      DangerId = DangerId,
+      // DangerId = DangerId,
       CategoryId = CategoryId,
       Description = Description,
     };
