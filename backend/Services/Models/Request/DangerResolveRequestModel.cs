5b1359eb6f0952badffae545f8e17e099715ae0e
Xeeija <19950592+xeeija@users.noreply.github.com>
2023-12-20 14:42:56 +0100

    129: remove CategoryId form DangerResolveRequestModel

--- backend/Services/Models/Request/DangerResolveRequestModel.cs
+++ backend/Services/Models/Request/DangerResolveRequestModel.cs
@@ -6,7 +6,7 @@ public class DangerResolveRequestModel : IRequestModel<DangerResolveRequest> {
   public Guid UserId { get; set; }
   public Guid DangerId { get; set; }
   public DateTime? Timestamp { get; set; }
-  public Guid? CategoryId { get; set; }
+  // public Guid? CategoryId { get; set; }
 
   public DangerResolveRequest ToEntity() {
     return new DangerResolveRequest {
