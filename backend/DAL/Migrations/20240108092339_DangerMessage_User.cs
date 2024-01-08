ac23888cbdd3addf21f4ec2600f1b74618264a95
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-01-08 10:35:57 +0100

    143: add User to DangerMessage

---
+++ backend/DAL/Migrations/20240108092339_DangerMessage_User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class DangerMessage_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "DangerMessage",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DangerMessage_UserId",
+                table: "DangerMessage",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DangerMessage_User_UserId",
+                table: "DangerMessage",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DangerMessage_User_UserId",
+                table: "DangerMessage");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DangerMessage_UserId",
+                table: "DangerMessage");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "DangerMessage");
+        }
+    }
+}
