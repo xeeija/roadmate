6b20827f9a427693299aaf882150d545031601a5
Xeeija <19950592+xeeija@users.noreply.github.com>
2024-01-10 23:32:25 +0100

    94: update RouteController and RouteService naming

--- backend/API/Controllers/RouteController.cs
+++ backend/API/Controllers/RouteController.cs
@@ -2,30 +2,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Services;
 using Services.Models.Request;
+using Services.Models.Response;
 using Route = DAL.Entities.Route;
 
 namespace API.Controllers;
 
 public class RouteController : BaseController<Route, RouteRequest> {
-  private readonly RouteService _routeService;
-  private readonly IHttpContextAccessor _accessor;
+  private readonly RouteService routeService;
   public RouteController(GlobalService service, IHttpContextAccessor accessor) : base(service.RouteService, accessor) {
-    _routeService = service.RouteService;
-    _accessor = accessor;
+    routeService = service.RouteService;
   }
   /// <summary>
   ///   Gets all Routes per User.
   /// </summary>
   /// <returns>All entities.</returns>
-  [HttpGet("user")]
-  public async Task<IActionResult> GetRoutesByUserId() {
-    var userIdClaim = _accessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier);
-    if (userIdClaim == null) {
-      return BadRequest("User ID claim not found.");
+  [HttpGet("User")]
+  public async Task<ActionResult<ItemResponseModel<List<Route>>>> GetRoutesByUserId() {
+    if (CurrentUser?.ID == null) {
+      return Forbid();
     }
 
-    var userId = Guid.Parse(userIdClaim.Value);
-    var routes = await _routeService.GetRoutesByUserId(userId);
-    return Ok(routes);
+    var routes = await routeService.GetRoutesByUserId(CurrentUser.ID);
+    return Ok(routes.ToList());
   }
 }
--- backend/Services/RouteService.cs
+++ backend/Services/RouteService.cs
@@ -5,11 +5,9 @@ using Microsoft.EntityFrameworkCore;
 namespace Services;
 
 public class RouteService : BaseService<Route> {
-  private readonly PostgresDbContext _context;
-  public RouteService(PostgresDbContext context) : base(context) {
-    _context = context;
-  }
-  public async Task<IEnumerable<Route>> GetRoutesByUserId(Guid userId) {
-    return await _context.Route.Where(r => r.UserId == userId).ToListAsync();
+  public RouteService(PostgresDbContext context) : base(context) { }
+
+  public async Task<List<Route>> GetRoutesByUserId(Guid userId) {
+    return await Context.Route.Where(r => r.UserId == userId).ToListAsync();
   }
 }
