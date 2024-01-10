7fba7c0dd33c4354c3e8a7006bcd3c67aceb65ce
paul <73228425+paulkunisch@users.noreply.github.com>
2024-01-10 12:39:05 +0100

    94: update get User-ID from Claim

--- backend/API/Controllers/RouteController.cs
+++ backend/API/Controllers/RouteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
 using Services;
 using Services.Models.Request;
 using Route = DAL.Entities.Route;
@@ -7,15 +8,23 @@ namespace API.Controllers;
 
 public class RouteController : BaseController<Route, RouteRequest> {
   private readonly RouteService _routeService;
+  private readonly IHttpContextAccessor _accessor;
   public RouteController(GlobalService service, IHttpContextAccessor accessor) : base(service.RouteService, accessor) {
     _routeService = service.RouteService;
+    _accessor = accessor;
   }
   /// <summary>
   ///   Gets all Routes per User.
   /// </summary>
   /// <returns>All entities.</returns>
-  [HttpGet("user/{userId}")]
-  public async Task<IActionResult> GetRoutesByUserId(Guid userId) {
+  [HttpGet("user")]
+  public async Task<IActionResult> GetRoutesByUserId() {
+    var userIdClaim = _accessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier);
+    if (userIdClaim == null) {
+      return BadRequest("User ID claim not found.");
+    }
+
+    var userId = Guid.Parse(userIdClaim.Value);
     var routes = await _routeService.GetRoutesByUserId(userId);
     return Ok(routes);
   }
